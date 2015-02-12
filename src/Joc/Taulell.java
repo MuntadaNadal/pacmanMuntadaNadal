f1eef09bb8a309653ee2e8b1ca5ca3dd5bf58048
soulerine <munti109@hotmail.com>
2015-02-12 16:06:37 +0100

    el primer comit

---
+++ src/Joc/Taulell.java
@@ -0,0 +1,49 @@
+package Joc;
+
+public class Taulell {
+	
+		private static char table[][] = new char[17][19];
+		
+		public Taulell(){
+			table= new char [][]{
+				"###################".toCharArray()
+				,"#........#........#".toCharArray()
+				,"#.##.###.#.###.##.#".toCharArray()
+				,"#.................#".toCharArray()
+				,"#.##.#.#####.#.##.#".toCharArray()
+				,"#....#...#...#....#".toCharArray()
+				,"####.###.#.###.####".toCharArray()
+				,"####.#.......#.####".toCharArray()
+				,"####...#####...####".toCharArray()
+				,"####.#.......#.####".toCharArray()
+				,"####.###.#.###.####".toCharArray()
+				,"#....#...#...#....#".toCharArray()
+				,"#.##.#.#####.#.##.#".toCharArray()
+				,"#.................#".toCharArray()
+				,"#.##.###.#.###.##.#".toCharArray()
+				,"#........#........#".toCharArray()
+				,"###################".toCharArray()
+				
+					
+			};
+			
+		}	
+		
+
+		
+
+		public static void mostrar_taulell() {
+
+			for (int i = 0; i <17; i++) {
+				for (int j = 0; j <19; j++) {
+					System.out.print("" + table[i][j]+ " ");
+				}
+				System.out.println("");
+			}
+		}
+		
+		
+
+	
+}
+
